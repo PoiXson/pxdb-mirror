36129c1c2f1f991a947c061dce91464688f52d75
lorenzop <lorenzo@poixson.com>
2017-02-10 06:51:19 -0500

    added empty dbCommand_ classes for check, update, import and export commands

---
+++ src/shell/dbCommand_Update.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * PoiXson pxdb - PHP Database Utilities Library
+ * @copyright 2004-2017
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\pxdb\shell;
+
+
+class dbCommand_Update extends dbCommands {
+
+
+
+	// returns true if successful
+	public function execute($pool, $tableName) {
+		
+		
+		
+		
+		
+	}
+
+
+
+}
