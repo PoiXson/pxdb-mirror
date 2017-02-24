ac0294c4902b3a4ac7a541dfa75a28d40e309379
lorenzop <lorenzo@poixson.com>
2017-02-23 21:53:40 -0500

    added getName() getSize() getNullable().. functions to dbField class

--- src/dbField.php
+++ src/dbField.php
@@ -60,9 +60,27 @@ class dbField {
 
 
 
+	// field name
+	public function getName() {
+		return $this->name;
+	}
+
+
+
+	// field size
+	public function getSize() {
+		return $this->size;
+	}
 	public function setSize($size) {
 		$this->size = $size;
 		return $this;
+	}
+
+
+
+	// nullable field
+	public function getNullable() {
+		return $this->nullable;
 	}
 	public function setNullable($nullable=TRUE) {
 		if ($nullable === NULL) {
@@ -72,9 +90,23 @@ class dbField {
 		}
 		return $this;
 	}
+
+
+
+	// default value
+	public function getDefault() {
+		return $this->defValue;
+	}
 	public function setDefault($defValue) {
 		$this->defValue = $defValue;
 		return $this;
+	}
+
+
+
+	// auto-increment
+	public function isAutoIncrement() {
+		return ($this->increment === TRUE);
 	}
 	public function setAutoIncrement($increment=TRUE) {
 		if ($increment === NULL) {
@@ -84,6 +116,13 @@ class dbField {
 		}
 		return $this;
 	}
+
+
+
+	// primary key
+	public function isPrimaryKey() {
+		return ($this->primary === TRUE);
+	}
 	public function setPrimaryKey($primary=TRUE) {
 		if ($primary === NULL) {
 			$this->primary = NULL;
@@ -91,6 +130,13 @@ class dbField {
 			$primary = ($primary === TRUE);
 		}
 		return $this;
+	}
+
+
+
+	// unique
+	public function isUnique() {
+		return ($this->unique === TRUE);
 	}
 	public function setUnique($unique=TRUE) {
 		if ($unique === NULL) {
