5e46ee5949560df8c586aff9ca66fecb9a0c5151
lorenzop <lorenzo@poixson.com>
2022-09-15 00:04:10 -0400

    updated header comments

--- src/dbTableSchema.php
+++ src/dbTableSchema.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types = 1);
 /*
  * PoiXson pxdb - PHP Database Utilities Library
- * @copyright 2004-2021
- * @license GPL-3
+ * @copyright 2004-2022
+ * @license AGPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
  * /
