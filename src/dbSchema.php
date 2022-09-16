25adb569794fdfe8e1bb4f202de56194e0e3192c
lorenzop <lorenzo@poixson.com>
2022-09-16 03:56:43 -0400

    finished renaming dbSchema class and cleaned up

--- src/dbSchema.php
+++ src/dbSchema.php
@@ -5,19 +5,17 @@
  * @license AGPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
 namespace pxn\pxdb;
 
 
-abstract class dbTableSchema extends dbTable {
+abstract class dbSchema {
 
 
 
-	public function __construct($pool, $tableName) {
-		parent::__construct($pool, $tableName);
+	public function __construct() {
 	}
 
 
 
 }
-*/
