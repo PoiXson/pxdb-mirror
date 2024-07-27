e50a429d7b30e1284e1f00a459962d6c214e89f0
lorenzop <lorenzo@poixson.com>
2024-07-27 00:17:35 -0400

    finished Command_InstallUpdate class

--- src/commands/Command_InstallUpdate.php
+++ src/commands/Command_InstallUpdate.php
@@ -16,8 +16,29 @@ class Command_InstallUpdate extends \pxn\phpShell\Command {
 
 
 	public function run(): int {
-//TODO
-return 1;
+		if (!\is_callable([$this->app, 'getDatabaseSchema']))
+			throw new \RuntimeException('function getDatabaseSchema() not found in class: '.\get_class($this->app));
+		$schema = \call_user_func([$this->app, 'getDatabaseSchema']);
+		$pool = $this->app->getUsersDB();
+		$tables_found = dbTools::GetTables($pool);
+		$count_added_tables      = 0;
+		$count_add_change_fields = 0;
+		foreach ($schema as $table) {
+			$table_name = $table->getTableName();
+			// create table
+			if (!\in_array(haystack: $tables_found, needle: $table_name)) {
+				dbTools::CreateTable($pool, $table);
+				$count_added_tables++;
+			}
+			// add/update fields
+			$count_add_change_fields += dbTools::UpdateTableFields($pool, $table);
+		}
+		if ($count_added_tables > 0)
+			echo "Added [ $count_added_tables ] tables\n";
+		if ($count_add_change_fields > 0)
+			echo "Added/Updated [ $count_add_change_fields ] fields\n";
+		echo "Finished\n";
+		return 0;
 	}
 
 
