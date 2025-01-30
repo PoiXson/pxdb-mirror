e9159f108a84d3a75f8ae879b3d9dcab5c4bee6d
lorenzop <lorenzo@poixson.com>
2025-01-30 02:24:40 -0500

    updated license headers and changing license to AGPLv3+ADD-PXN-V1 addendum

--- src/dbTools.php
+++ src/dbTools.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 /*
  * PoiXson pxdb - PHP Database Utilities Library
- * @copyright 2004-2024
- * @license AGPL-3
+ * @copyright 2004-2025
+ * @license AGPLv3+ADD-PXN-V1
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
  */
